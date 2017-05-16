6fbd05e0c1a39157c6c08ab63cb5c9132260cd29
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-16 11:32:49 -0700

    second heroku update

--- db/migrate/20170516033639_rename_chairs_table.rb
+++
@@ -1,5 +0,0 @@
-class RenameChairsTable < ActiveRecord::Migration[5.0]
-  def change
-    rename_table :chairs, :chairs
-  end
-end
--- db/migrate/20170516033943_remove_seating_from_chairs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSeatingFromChairs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :chairs, :seating
-  end
-end
