801397a310b33a69eef35e22f62388f4e9d1a9fc
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-16 12:15:45 -0700

    style navbar and hero img/banner

--- frontend/components/search.jsx
+++ frontend/components/search.jsx
@@ -3,13 +3,15 @@ import ChairMap from './chair_map';
 import ChairIndex from './chair_index';
 import FilterForm from './filter_form';
 import ChairShowContainer from './chair_show_container';
+import { Link } from 'react-router-dom';
 
 const Search = ({chairs, fetchAllChairs, updateFilter}) => (
   <main>
     <img id='bannerImg' src="https://wallpaperlayer.com/img/2015/8/leather-chair-wallpaper-8869-9199-hd-wallpapers.jpg" />
     <div id='bannerContainer'>
       <h1 id='banner'>Find Your Perfect Chair</h1>
-      <h2 id='subHeader'>SF real estate is heating up. But these chairs are comfier than any bed.</h2>
+      <h2 id='subHeader'>SF real estate is heating up. Just sleep in these chairs.</h2>
+      <Link to='/login' className='demo'>Demo Log In</Link>
     </div>
 
     <FilterForm
