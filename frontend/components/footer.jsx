e53181067176ddde5ffdab990551b0adcd6ca7f1
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-17 18:58:09 -0700

    style social icons

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,18 +3,33 @@ import { Link } from 'react-router-dom';
 
 const Footer = (props) => (
   <section className='footer'>
-    <div className='footerLinks'>
-      <a href='#'>ABOUT</a>
-      <a href='#'>SAFETY</a>
-      <a href='#'>SUPPORT</a>
-      <a href='#'>BLOG</a>
-      <a href='#'>SHOP</a>
+    <div className='topLinks'>
+      <div className='footerLinks'>
+        <a href='#'>ABOUT</a>
+        <a href='#'>SAFETY</a>
+        <a href='#'>SUPPORT</a>
+        <a href='#'>BLOG</a>
+        <a href='#'>SHOP</a>
+      </div>
+      <div className='languageDiv'>
+        <select>
+          <option>English</option>
+          <option>Javascript</option>
+        </select>
+      </div>
     </div>
-    <div className='languageDiv'>
-      <select>
-        <option>English</option>
-        <option>Javascript</option>
-      </select>
+    <div className='midLinks'>
+      <div>
+        <p>CONNECT WITH US</p>
+        <a href='#' id='fb' className='fa fa-facebook socialIcon'></a>
+        <a href='#' className='fa fa-twitter socialIcon'></a>
+      </div>
+      <div>
+        <p>MOBILE APPS</p>
+        <a href='#' className='fa fa-apple socialIcon'></a>
+        <a href='#' className='fa fa-android socialIcon'></a>
+
+      </div>
     </div>
   </section>
 );
