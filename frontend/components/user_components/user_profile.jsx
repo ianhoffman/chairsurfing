40b4bb7eb3f8bba6cd2e7f6dd60f206a3936b621
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-18 18:48:26 -0700

    attempt to animate button in a fancy way, sort of give up

--- frontend/components/user_components/user_profile.jsx
+++ frontend/components/user_components/user_profile.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
-
+const ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class UserProfile extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    // $('.createChairButton').animate()
+  }
+
   render() {
     const {currentUser} = this.props;
 
     return(currentUser.chair === 'null') ? (
-      <a className='createChairButton'>
-        Create your chair!
-      </a>
+      <section className='chairButtonContainer'>
+        <ReactCSSTransitionGroup
+          transitionName='auto'
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500}>
+            <a className='createChairButton button button-blue'>
+              Create your chair!
+            </a>
+        </ReactCSSTransitionGroup>
+      </section>
     ) : (
       <a>
         'YOU HAVE A CHAIR'
