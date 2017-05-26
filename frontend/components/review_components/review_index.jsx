b82c6856c1f282c1cfc2ebb3e1ab9aa062499bc5
Ian Hoffman <ianhoffman10@gmail.com>
2017-05-25 22:49:16 -0700

    debug review form to render form when its not your chair

--- frontend/components/review_components/review_index.jsx
+++ frontend/components/review_components/review_index.jsx
@@ -17,34 +17,16 @@ class ReviewIndex extends React.Component {
       reviews,
       deleteReview
     } = this.props;
-    const chairId = this.props.match.params.chairId;
+    const chairId = parseInt(this.props.match.params.chairId);
 
-    if(Object.keys(reviews).length === 0) {
-      if(currentUser !== null &&
-        currentUser.chair !== 'null' &&
-          currentUser.chair_id != chairId) {
-            return(
-              <div className='no-reviews'>
-                <div>
-                  There are no reviews...
-                </div>
-                <ReviewForm
-                  createReview={createReview}
-                  userId={currentUser.id}
-                  chairId={chairId}
-                  errors={errors}
-                  />
-              </div>
-            );
-      } else {
+    if(currentUser === null || currentUser.chair_id === chairId) {
+      if(Object.keys(reviews).length === 0) {
         return(
           <div className='no-reviews'>
             <div>Your chair has no reviews yet!</div>
           </div>
         );
-      }
-    } else {
-      if(currentUser === null) {
+      } else {
         return(
           <div className='review-index'>
             <ul className='review-list'>
@@ -52,41 +34,36 @@ class ReviewIndex extends React.Component {
                 <ReviewListItem
                   key={`review${key}`}
                   review={reviews[key]} />
-              )
-            )}
-          </ul>
-        </div>
-        );
-      } else {
-        return(
-          <div className='review-index'>
-            <ul className='review-list'>
-              {Object.keys(reviews).map(key => (
-                <ReviewListItem
-                  key={`review${key}`}
-                  review={reviews[key]}
-                  currentUser={currentUser}
-                  deleteReview={deleteReview} />
-              )
-            )}
-          </ul>
-          { currentUser.chair !== 'null' &&
-            currentUser.chairId == chairId ? (
-              <ReviewForm
-                createReview={createReview}
-                userId={currentUser.id}
-                chairId={chairId}
-                errors={errors}
-                />
-            ) : (
-              ''
-            ) }
+                  )
+                )
+              }
+            </ul>
           </div>
-
-        )
+        );
       }
+    } else {
+      return(
+        <div className='review-index'>
+          <ul className='review-list'>
+            {Object.keys(reviews).map(key => (
+              <ReviewListItem
+                key={`review${key}`}
+                currentUser={currentUser}
+                deleteReview={deleteReview}
+                review={reviews[key]} />
+              )
+            )}
+          </ul>
+          <ReviewForm
+            errors={errors}
+            createReview={createReview}
+            userId={currentUser.id}
+            chairId={chairId} />
+        </div>
+      );
     }
   }
+
 }
 
 export default withRouter(ReviewIndex);
